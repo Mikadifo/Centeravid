fc9bfa5deb86210182e7caa7ac42f3bb6ab718a2
Mikadifo <mikadifopadillaheredia@gmail.com>
2022-09-26 07:55:10 -0400

    [add] basic navbar structure

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,13 +1,65 @@
 import { Link } from 'react-router-dom';
+import logo from './../../logo.png';
+import './NavBar.css';
 
 const NavBar = () => {
     return (
-        <nav>
-            <Link to="/">go H</Link>
-            <Link to="/terapias">go T</Link>
-            <Link to="/instalacion">go I</Link>
-            <Link to="/cuerpo_medico">go M</Link>
-            <Link to="/contacto">go C</Link>
+        <nav className="navbar navbar-expand-lg">
+            <div className="container-fluid p-0">
+                <Link className="navbar-brand p-0 m-0" to="/">
+                    <img
+                        src={logo}
+                        alt="Logo"
+                        width="64"
+                        height="64"
+                        className="d-inline-block align-text-top"
+                    />
+                </Link>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarNav"
+                    aria-controls="navbarNav"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon" />
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <Link
+                                className="nav-link active"
+                                aria-current="page"
+                                to="/"
+                            >
+                                Inicio
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/terapias">
+                                Terapias
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/instalacion">
+                                Instalación
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/cuerpo_medico">
+                                Cuerpo Médico
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/contacto">
+                                Contacto
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </nav>
     );
 };

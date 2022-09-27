051ac7e2977772dd0fb3ffc0c7dc82bb43810ccd
Mikadifo <mikadifopadillaheredia@gmail.com>
2022-09-27 17:18:44 -0400

    [add] therapy cards

--- src/components/therapyCard/TherapyCard.js
+++ src/components/therapyCard/TherapyCard.js
@@ -1,5 +1,17 @@
+import './TherapyCard.css';
+
 const TherapyCard = ({ img, title, text }) => {
-    return <>TherapyCard</>;
+    return (
+        <div className="therapy-card-container row">
+            <div className="col p-0 my-auto">
+                <img src={img} alt={title} />
+            </div>
+            <div className="col p-0 my-auto">
+                <h3>{title}</h3>
+                <p>{text}</p>
+            </div>
+        </div>
+    );
 };
 
 export default TherapyCard;
--- src/views/therapies/Therapies.js
+++ src/views/therapies/Therapies.js
@@ -1,5 +1,7 @@
 import TherapyCard from '../../components/therapyCard/TherapyCard';
 import TopCard from '../../components/topCard/TopCard';
+import imgExample from './../../resources/imgs/home_img.jpeg'; //TODO: get real one
+import './Therapies.css';
 
 const Therapies = () => {
     return (
@@ -8,7 +10,15 @@ const Therapies = () => {
                 title={'Terapias'}
                 subtitle={'Ofrecemos las mejores terapias de rehabilitación'}
             />
-            <TherapyCard />
+            <div className="therapies">
+                <TherapyCard
+                    img={imgExample}
+                    title={'Example'}
+                    text={
+                        'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.'
+                    }
+                />
+            </div>
         </>
     );
 };

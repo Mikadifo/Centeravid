c159ca41b549a074cc5465f1fc02885651a7022f
Mikadifo <mikadifopadillaheredia@gmail.com>
2022-09-27 07:45:27 -0400

    [add] html varibales for paragraph, added home text card styles

---
+++ src/constants/homeTexts.js
@@ -0,0 +1,5 @@
+export const aboutUsTitle = '¿Quiénes somos?';
+export const aboutUs = [
+    'CENTERAVID, fue creada con el fin de dar asistencia terapéutica a la población adulta hombres a partir de 18 años hasta los 65 años de edad, con problemas de drogodependencia y a sus familias, además de tener actividades en los aspectos básicos que propone la OMS. Y LA O.P.S. En los campos de prevención, asistencia y rehabilitación, así como también en el ámbito de reinserción social, educativo, laboral y familiar',
+    '"Porque solo podemos conservar lo que tenemos en la medida en que compartamos con otras personas"',
+];

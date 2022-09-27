c1343c14cc98011f05f950ab2313eafb1b16f7e8
Mikadifo <mikadifopadillaheredia@gmail.com>
2022-09-27 19:17:26 -0400

    [add] plants imgs

--- src/resources/info/plants.js
+++ src/resources/info/plants.js
@@ -1,45 +1,52 @@
-import img from './../imgs/T_espiritual.jpeg';
+import P_gim from './../imgs/P_gim.jpeg';
+import P_coc from './../imgs/P_coc.jpeg';
+import P_com from './../imgs/P_com.jpeg';
+import P_con from './../imgs/P_con.jpeg';
+import P_jar from './../imgs/P_jar.jpeg';
+import P_dorm from './../imgs/P_dorm.jpeg';
+import P_pat from './../imgs/P_pat.jpeg';
+import P_sala from './../imgs/P_sala.jpeg';
 
 const plants = [
     {
         id: 'P_Gim',
         title: 'Gimansio',
-        img: img,
+        img: P_gim,
     },
     {
         id: 'P_Dorm',
         title: 'Dormitorios',
-        img: img,
+        img: P_dorm,
     },
     {
         id: 'P_Jar',
         title: 'Jardín',
-        img: img,
+        img: P_jar,
     },
     {
         id: 'P_Con',
         title: 'Consultorios',
-        img: img,
+        img: P_con,
     },
     {
         id: 'P_Pat',
         title: 'Patio',
-        img: img,
+        img: P_pat,
     },
     {
         id: 'P_Sala',
         title: 'Sala de reuniones',
-        img: img,
+        img: P_sala,
     },
     {
         id: 'P_Coc',
         title: 'Cocina',
-        img: img,
+        img: P_coc,
     },
     {
         id: 'P_Com',
         title: 'Comedor',
-        img: img,
+        img: P_com,
     },
 ];
 

ff41a1826347a110601b55b0e0f38cf5af8d0d2d
Mikadifo <mikadifopadillaheredia@gmail.com>
2022-09-27 17:27:12 -0400

    [mod] therapies img left right interwingin

--- src/components/therapyCard/TherapyCard.js
+++ src/components/therapyCard/TherapyCard.js
@@ -1,9 +1,13 @@
 import './TherapyCard.css';
 
-const TherapyCard = ({ img, title, text }) => {
+const TherapyCard = ({ img, title, text, imgLeft = false }) => {
     return (
         <div className="therapy-card-container row">
-            <div className="col p-0 my-auto">
+            <div
+                className={`col p-0 my-auto ${
+                    !imgLeft && 'order-last text-end'
+                }`}
+            >
                 <img src={img} alt={title} />
             </div>
             <div className="col p-0 my-auto">

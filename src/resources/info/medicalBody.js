48e40028240809df7d9ed79b12d5a0b77b507d6c
Mikadifo <mikadifopadillaheredia@gmail.com>
2022-09-28 20:13:28 -0400

    [mod] medical body cards cover imgs

--- src/resources/info/medicalBody.js
+++ src/resources/info/medicalBody.js
@@ -1,47 +1,53 @@
-import img from './../imgs/P_coc.jpeg';
+import M_isa from './../imgs/M_isa.jpeg';
+import M_diana from './../imgs/M_diana.jpeg';
+import M_isma from './../imgs/M_isma.jpeg';
+import M_esteban from './../imgs/M_esteban.jpeg';
+import M_xavi from './../imgs/M_xavi.jpeg';
+import M_edwin from './../imgs/M_edwin.jpeg';
+import M_sady from './../imgs/M_sady.jpeg';
 
 const medicalBody = [
     {
         id: 'M_Isa',
         name: 'Dra. Isabel Padilla',
         role: 'Directora Técnica',
-        img: img,
+        img: M_isa,
     },
     {
         id: 'M_Diana',
         name: 'Lcda. Diana Auquilla',
         role: 'Directora General',
-        img: img,
+        img: M_diana,
     },
     {
         id: 'M_Isma',
         name: 'Ismael Sarmiento',
         role: 'Psicólogo Clínico',
-        img: img,
+        img: M_isma,
     },
     {
         id: 'M_Edwin',
         name: 'Edwin Baculima',
         role: 'Psicólogo Clínico',
-        img: img,
+        img: M_edwin,
     },
     {
         id: 'M_Xavi',
         name: 'Xavier Méndez',
         role: 'Psicólogo Clínico',
-        img: img,
+        img: M_xavi,
     },
     {
         id: 'M_Esteban',
         name: 'Esteban Sarmiento',
         role: 'Terapia Vivencial',
-        img: img,
+        img: M_esteban,
     },
     {
         id: 'M_Sady',
         name: 'Dra. Sady Tello',
         role: 'Psiquiatra',
-        img: img,
+        img: M_sady,
     },
 ];
 

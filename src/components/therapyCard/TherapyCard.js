1e3df824afcae6d442a216bb633ada3e40b689ba
Mikadifo <mikadifopadillaheredia@gmail.com>
2022-10-08 18:10:27 -0400

    [feat] therapies cards responsive

--- src/components/therapyCard/TherapyCard.js
+++ src/components/therapyCard/TherapyCard.js
@@ -4,13 +4,13 @@ const TherapyCard = ({ therapy, imgLeft = false }) => {
     return (
         <div className="therapy-card-container row" data-testid={therapy.id}>
             <div
-                className={`col p-0 my-auto ${
-                    !imgLeft && 'order-last text-end'
+                className={`col-12 col-md pb-4 pe-0 ps-0 p-md-0 my-auto text-center text-md-start ${
+                    !imgLeft && 'order-md-last text-md-end margin-end'
                 }`}
             >
                 <img src={therapy.img} alt={therapy.title} />
             </div>
-            <div className="col p-0 my-auto">
+            <div className="col-12 col-md p-0 my-auto">
                 <h3>{therapy.title}</h3>
                 <p>{therapy.text}</p>
             </div>
